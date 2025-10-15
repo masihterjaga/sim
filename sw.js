964fca73a7ec7a404a03ecf44f238ab7458f2ebe
masihterjaga <me.masyan@gmail.com>
2025-10-15 14:20:51 +0700

    update sw.js

--- sw.js
+++ sw.js
@@ -20,8 +20,7 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', event => {
-  self.skipWaiting(); // Force install langsung tanpa tunggu tab ditutup
-  
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
     .then(cache => cache.addAll(urlsToCache))
@@ -35,7 +34,7 @@ self.addEventListener('activate', event => {
       names.filter(name => name !== CACHE_NAME)
       .map(name => caches.delete(name))
     ))
-    .then(() => self.clients.claim()) // Ambil alih semua tabs
+    .then(() => self.clients.claim())
   );
 });
 
