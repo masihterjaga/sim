77b180fe8459133671020a06d8b4257ef1e85932
masihterjaga <me.masyan@gmail.com>
2026-06-16 19:44:29 +0700

    update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,6 @@ const CACHE_NAME = 'rox-calc-v1.2.4.88';
 const urlsToCache = [
   '/sim/',
   '/sim/index.html',
-  '/sim/roxtimizer.html',
   '/sim/physical-suno-mammonite-se.html',
   '/sim/style/v1.1.5.css',
   '/sim/style/roxtimizer/main.css',
